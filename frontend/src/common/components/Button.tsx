f32f177b4c92e9ca6303dc5c184f0c4672c61380
Giovanni Angelini <giovanniangelini@Giovannis-MacBook-Pro.local>
2022-02-26 01:25:11 -0500

    Button

--- frontend/src/common/components/Button.tsx
+++ frontend/src/common/components/Button.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import {red} from '../../theme/ColorConstants';
 
-export const Button = () => {
-  return <div>Button</div>;
+const StyledRoundButton = styled.input`
+  background-color: ${red.light};
+  border: none;
+  border-radius: 24px;
+  width: 42px;
+  transition: background-color 200ms linear;
+
+  &:hover {
+    transition: background-color 200ms linear;
+    background-color: ${red.normal};
+  }
+
+  &:active {
+    box-shadow: 0 0 8px rgb(0 0 0 / 0.75);
+  }
+`;
+
+interface RoundButtonProps {
+  onClick: () => void;
+}
+
+export const RoundButton = ({onClick}): RoundButtonProps => {
+  return (
+    <StyledRoundButton
+      onClick={onClick}
+      type="button"
+      value="Go!"
+    ></StyledRoundButton>
+  );
 };
--- frontend/src/common/components/SearchBar.tsx
+++ frontend/src/common/components/SearchBar.tsx
@@ -1,17 +1,26 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
+import {getRequest} from '../../apiUtils/api';
 import {gray, red} from '../../theme/ColorConstants';
+import {RoundButton} from './Button';
+
+const SearchBarWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
 
 const StyledInputWrapper = styled.div`
   width: 40%;
   height: 40px;
-  min-width: 200px;
+  min-width: 350px;
   border-radius: 24px;
   border: 1px solid ${gray.normal};
   display: flex;
   justify-content: center;
   align-items: center;
   transition: border 200ms linear;
+  margin: 0 16px;
   ${({isFocused}) => isFocused && `border: 1px solid ${red.dark};`}
 
   &:hover {
@@ -21,12 +30,18 @@ const StyledInputWrapper = styled.div`
 `;
 
 const StyledInput = styled.input`
-  width: 95%;
-  height: 95%;
+  width: 90%;
+  height: 90%;
   border: none;
   padding: 0;
   outline: none;
+  font-size: 18px;
 `;
+
+const onClick = (searchValue: string) => {
+  // getRequest('http://localhost:8090/games');
+  console.log(searchValue);
+};
 
 interface Props {
   searchType: string;
@@ -34,14 +49,19 @@ interface Props {
 
 export const SearchBar = ({searchType}): Props => {
   const [isFocused, setIsFocused] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
 
   return (
-    <StyledInputWrapper isFocused={isFocused}>
-      <StyledInput
-        placeholder={`Search for ${searchType}...`}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-      />
-    </StyledInputWrapper>
+    <SearchBarWrapper>
+      <StyledInputWrapper isFocused={isFocused}>
+        <StyledInput
+          placeholder={`Search for ${searchType}...`}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </StyledInputWrapper>
+      <RoundButton onClick={() => onClick(searchValue)} />
+    </SearchBarWrapper>
   );
 };
