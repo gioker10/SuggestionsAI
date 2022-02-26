4b69137fee5ea9eee65da8007d28e59fa64accc8
Giovanni Angelini <giovanniangelini@Giovannis-MacBook-Pro.local>
2022-02-26 00:43:53 -0500

    bleh its really late and im tired

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,8 +3,16 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'google', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'google',
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parser: '@typescript-eslint/parser',
+  ignorePatterns: ['*.html'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -15,6 +23,9 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': ['error'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'react/prop-types': 0,
   },
   settings: {
     react: {
